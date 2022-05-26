01ba663262b65b99cfcfd5d264fb0f830139af11
nguyen y nhi <nhi.nguyeny.sistrain@gmail.com>
2022-05-27 01:10:53 +0700

    project update

--- lib/data/repositories/authentication_repo.dart
+++ lib/data/repositories/authentication_repo.dart
@@ -1,12 +1,12 @@
 import 'package:cha_qaqc_department_mobile_app/data/data_source/fetch_token.dart';
-import 'package:cha_qaqc_department_mobile_app/domain/entities/user.dart';
+import 'package:cha_qaqc_department_mobile_app/data/model/user_model.dart';
 import 'package:cha_qaqc_department_mobile_app/domain/repositories/authentication_repository.dart';
 
 class AuthenticationRepoimpl implements AuthenticationRepository {
   final FetchTokenService fetchTokenService;
   AuthenticationRepoimpl(this.fetchTokenService);
   @override
-  Future<User> authenticate(String username, String password) async {
+  Future<UserModel> authenticate(String username, String password) async {
     return fetchTokenService.getToken(username, password);
   }
 }
